2c041cd9216cdcb5cd72cdec02e4124a52344236
Lathan Hay <lathan@Lathans-Air.localdomain>
2025-05-15 16:45:58 -0700

    fft

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,83 @@
-//! Records a WAV file (roughly 3 seconds long) using the default input device and config.
-//!
-//! The input data is recorded to "$CARGO_MANIFEST_DIR/recorded.wav".
-
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{FromSample, Sample};
-use std::fs::File;
-use std::io::BufWriter;
 use std::sync::{Arc, Mutex};
 
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use rustfft::num_complex::Complex;
+
 fn main() -> Result<(), anyhow::Error> {
+    let audio_data = Arc::new(Mutex::new(Vec::new()));
+    let audio_data_clone = Arc::clone(&audio_data);
+
     let host = cpal::default_host();
 
     // Set up the input device and stream with the default input config.
     let device = host.default_input_device()
         .expect("failed to find input device");
-
     println!("Input device: {}", device.name()?);
 
-    let config = device
-        .default_input_config()
-        .expect("Failed to get default input config");
-    println!("Default input config: {:?}", config);
+    let config = device.default_input_config()
+        .expect("No supported I8 output format");
 
-    // The WAV file we're recording to.
-    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
-    let spec = wav_spec_from_config(&config);
-    let writer = hound::WavWriter::create(PATH, spec)?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
-
-    // A flag to indicate that recording is in progress.
     println!("Begin recording...");
-
-    // Run the input stream on a separate thread.
-    let writer_2 = writer.clone();
 
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
     };
 
-    let stream = match config.sample_format() {
-        cpal::SampleFormat::I8 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i8, i8>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::I16 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::I32 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i32, i32>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::F32 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
-            err_fn,
-            None,
-        )?,
-        sample_format => {
-            return Err(anyhow::Error::msg(format!(
-                "Unsupported sample format '{sample_format}'"
-            )))
-        }
-    };
+    let stream = device.build_input_stream(
+        &config.into(),
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            let mut buffer = audio_data_clone.lock().unwrap();
+            buffer.extend_from_slice(data);
+        }, 
+        err_fn,
+        None
+    ).unwrap();
 
-    stream.play()?;
+    stream.play().unwrap();
 
-    // Let recording go for roughly three seconds.
     std::thread::sleep(std::time::Duration::from_secs(3));
+
     drop(stream);
-    writer.lock().unwrap().take().unwrap().finalize()?;
-    println!("Recording {} complete!", PATH);
+
+    println!("No longer capturing stream");
+
+    let spectogram = build_spectogram(Arc::clone(&audio_data));
+    visualize_spectogram(&spectogram);
+
     Ok(())
 }
 
-fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
-    if format.is_float() {
-        hound::SampleFormat::Float
-    } else {
-        hound::SampleFormat::Int
-    }
-}
-
-fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec {
-    hound::WavSpec {
-        channels: config.channels() as _,
-        sample_rate: config.sample_rate().0 as _,
-        bits_per_sample: (config.sample_format().sample_size() * 8) as _,
-        sample_format: sample_format(config.sample_format()),
-    }
-}
-
-type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
-
-fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
-where
-    T: Sample,
-    U: Sample + hound::Sample + FromSample<T>,
-{
-    if let Ok(mut guard) = writer.try_lock() {
-        if let Some(writer) = guard.as_mut() {
-            for &sample in input.iter() {
-                let sample: U = U::from_sample(sample);
-                writer.write_sample(sample).ok();
-            }
+fn visualize_spectogram(data: &Vec<[Complex<f32>; 128]>) {
+    for time_slice in data {
+        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        for point in time_slice {
+            println!("{}", point);
         }
+        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
     }
+}
+
+fn build_spectogram(data: Arc<Mutex<Vec<f32>>>) -> Vec<[Complex<f32>; 128]>  {
+    //TODO might want to deal with the remainder
+    let mut time_sliced_data: Vec<[Complex<f32>; 128]> = data
+        .lock()
+        .unwrap()
+        .chunks_exact(128)
+        .map(|chunk| {
+            let mut buf = [Complex::new(0.0, 0.0); 128];
+            for (i, &real) in chunk.iter().enumerate() {
+                buf[i].re = real;
+            }
+            buf
+        })
+        .collect();
+
+    let mut planner = rustfft::FftPlanner::new();
+    let fft = planner.plan_fft_forward(128);
+
+    for time_slice in &mut time_sliced_data {
+        fft.process(time_slice);
+    } 
+
+    time_sliced_data
 }
