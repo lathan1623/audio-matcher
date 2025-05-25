90f47cbec77f61c0d92c4cdd2464e3de472d45cd
Lathan Hay <lathan@Lathans-Air.lan>
2025-05-25 00:01:15 -0500

    find high points start

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::sync::{Arc, Mutex};
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use rustfft::num_complex::Complex;
+use rustfft::num_complex::{Complex, ComplexFloat};
 
 fn main() -> Result<(), anyhow::Error> {
+
     let audio_data = Arc::new(Mutex::new(Vec::new()));
     let audio_data_clone = Arc::clone(&audio_data);
 
@@ -42,29 +43,55 @@ fn main() -> Result<(), anyhow::Error> {
     println!("No longer capturing stream");
 
     let spectogram = build_spectogram(Arc::clone(&audio_data));
-    visualize_spectogram(&spectogram);
+    find_key_points(&spectogram);
 
     Ok(())
 }
 
-fn visualize_spectogram(data: &Vec<[Complex<f32>; 128]>) {
+fn find_key_points(data: &Vec<[Complex<f32>; 1024]>) {
+    let freq_ranges = [0, 10, 20, 40, 160, 511];
     for time_slice in data {
-        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-        for point in time_slice {
-            println!("{}", point);
+        let mut cur_freq_index = 0;
+        let mut max_points = [f32::NEG_INFINITY; 6];
+        let mut max_freqs = [0; 6];
+        for freq in 0..511 {
+            let mag = 20.0 * time_slice[freq].abs().max(1e-10).log10();
+            if freq > freq_ranges[cur_freq_index] {
+                cur_freq_index += 1
+            }
+            if mag > max_points[cur_freq_index] {
+                max_points[cur_freq_index] = mag;
+                max_freqs[cur_freq_index] = freq
+            }
+            
+
+            
         }
-        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        for freq in max_freqs {
+            println!("{}", freq)
+        }
     }
 }
 
-fn build_spectogram(data: Arc<Mutex<Vec<f32>>>) -> Vec<[Complex<f32>; 128]>  {
+fn visualize_spectogram(data: &Vec<[Complex<f32>; 1024]>) {
+    for time_slice in data {
+        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        for freq in 0..1023 {
+            let mag = 20.0 * time_slice[freq].abs().max(1e-10).log10();
+            println!("{}", mag);
+        }
+        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    }
+}
+
+fn build_spectogram(data: Arc<Mutex<Vec<f32>>>) -> Vec<[Complex<f32>; 1024]>  {
     //TODO might want to deal with the remainder
-    let mut time_sliced_data: Vec<[Complex<f32>; 128]> = data
+    let mut time_sliced_data: Vec<[Complex<f32>; 1024]> = data
         .lock()
         .unwrap()
-        .chunks_exact(128)
+        .chunks_exact(1024)
         .map(|chunk| {
-            let mut buf = [Complex::new(0.0, 0.0); 128];
+            let mut buf = [Complex::new(0.0, 0.0); 1024];
             for (i, &real) in chunk.iter().enumerate() {
                 buf[i].re = real;
             }
@@ -73,7 +100,7 @@ fn build_spectogram(data: Arc<Mutex<Vec<f32>>>) -> Vec<[Complex<f32>; 128]>  {
         .collect();
 
     let mut planner = rustfft::FftPlanner::new();
-    let fft = planner.plan_fft_forward(128);
+    let fft = planner.plan_fft_forward(1024);
 
     for time_slice in &mut time_sliced_data {
         fft.process(time_slice);
